d2e7c432fcfd2564d35dac0142d2a1d10f6069ee
Roland <161763507+RocknRolly@users.noreply.github.com>
2024-03-30 21:55:30 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 let siluette = document.getElementById("content");
 let img1 = document.getElementById("link1");
+let ujKepUrl = "camera-transparent-lila.png";
+let ujKep = new Image();
+ujKep.src = ujKepUrl;
+
+let regiregiKep = "camera-transparent.png";
+let regikep = new Image();
+regikep.src = regiregiKep;
 
 const showSiluette = () => {
   siluette.classList.add("show");
+  let lilaKep = document.getElementById("kamera");
+  lilaKep.src = ujKep.src;
 };
 
 const hideSiluette = () => {
   siluette.classList.remove("show");
+  let lilaKep = document.getElementById("kamera");
+  lilaKep.src = regikep.src;
 };
 
 img1.addEventListener("mousemove", showSiluette);
